309e29d4e5c73555d880bf156e021c2e78b19818
xChipseq <sztabekminecraft@gmail.com>
2025-10-12 15:28:18 +0200

    fix toum appearance speed getting applied twice

--- ChaosTokens/Patches/SpeedPatch.cs
+++ ChaosTokens/Patches/SpeedPatch.cs
@@ -8,11 +8,8 @@ namespace ChaosTokens.Patches;
 [HarmonyPatch(typeof(LogicOptions), nameof(LogicOptions.GetPlayerSpeedMod))]
 public static class PlayerSpeedPatch
 {
-    // ReSharper disable once InconsistentNaming
     public static void Postfix(PlayerControl pc, ref float __result)
     {
-        __result *= pc.GetAppearance().Speed;
-        
         if (pc.TryGetModifier<TokenSpeed>(out var tokenSpeed))
         {
             __result *= tokenSpeed.Speed;
